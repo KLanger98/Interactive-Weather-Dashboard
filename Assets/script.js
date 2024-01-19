b6862c50c41478f897e1333252b2e99478835133
KL <karl.w.langer@gmail.com>
2024-01-19 15:11:03 +1030

    Added function to check if inputted city name is valid

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,47 @@
 const weatherAPI = "6b83faa0314e5970068d0ef9b2e007ee"
 
 
-
-//TODO: Add a function that fires when a search is submitted 
+//Create event listeners for submission and enter keypress
 $('#searchBtn').on('click', searchBtnClick);
+$('#searchInput').on('keypress', function(event){
+    if(event.key === "Enter"){
+        searchBtnClick();
+    }
+})
 
-
-function searchBtnClick(event){
+//Create a function that checks the validity of the inputted data by checking for an empty input and checking if the city exists
+function searchBtnClick(){
     let cityName = $('#searchInput').val()
-    //Check if city exists 
+
+    if(cityName == ""){
+        let noText = $('<h4>').text('Please add text to the search bar').css('color', 'red');
+        $('#searchBar').append(noText);
+        setTimeout(function(){
+            $(noText).remove()
+        }, 4000)
+        return;
+    }
+
+    coordinatesFetch(cityName)
+        .then(response =>{
+            if(!response.ok){
+                let error = $('<h4>').text('City does not exist').css('color', 'red').addClass('errorText')
+                $('#searchBar').append(error);
+                setTimeout(function(){
+                    $(error).remove()
+                }, 4000)
+                return;
+            }
+            runSearch();
+        })
+    
+}
+
+//Once valid, add the search to localStorage and reload the page 
+function runSearch(){
+    let cityName = $('#searchInput').val()
+    $('#searchInput').val(' ')
+
     //Add city to local Storage 
     let previousSearches = JSON.parse(localStorage.getItem('previousSearches'));
 
@@ -18,31 +51,11 @@ function searchBtnClick(event){
     localStorage.setItem('previousSearches', JSON.stringify(previousSearches))
     
     //clear all previous searches before re rendering
-    let previousSearchDiv = $('#previousSearches');
-    previousSearchDiv.empty();
+    $('#previousSearches').empty();
 
     loadPreviousSearches();
-
-
-    //
-}
-
-//TODO: Add the search to the list of previously searched cities and make it active and project contents onto dashboard 
-
-//TODO: Remove last previously searched item
-
-//TODO: Generate 5 day forecast
-
-//TODO: Generate current day forecast
-
-//TODO: Load minor infomation for list items
-
-//TODO: Make an API call 
-
-function appendNewSearch(){
-    let previousSearches = JSON.parse(localStorage.getItem('previousSearches'));
-
-    console.log(previousSearches);
+    $('#mainContainer').empty();
+    loadMainBoard();
 }
 
 function loadPreviousSearches(){
@@ -54,25 +67,51 @@ function loadPreviousSearches(){
         localStorage.setItem("previousSearches", JSON.stringify(previousSearches))
     }
     let prevSearchDiv = $('#previousSearches');
+    prevSearchDiv.empty();
+
+    let prevSearchHeading = $('<h3>').text('Previous Searches').css('color', 'white').addClass('ms-3')
+    prevSearchDiv.append(prevSearchHeading)
 
     for(let i = 0; i < previousSearches.length; i++){
-        let anchor = $('<a>').addClass('list-group-item list-group-item-action').attr('href', '#tab')
+        let anchor = $('<a>').addClass('list-group-item list-group-item-action list-group-item-primary').attr('href', '#tab')
         if(i == 0){
             anchor.addClass('active');
         }
         let body = $('<div>').addClass("d-flex w-100 justify-content-between");
         let p = $('<p>').addClass('mb-1');
-        let smallText = $('<small>').addClass('text-body-secondary').attr('id', 'weatherDescription');
-        let innerSmall = $('<div>').text('sometime');
+        let smallText = $('<small>').addClass('text-body-secondary').attr('id', 'weatherDescription').css('color', 'white');
         let heading = $('<h5>').text(previousSearches[i]);
 
+        anchor.on('click', changeActive)
+
         anchor.append(body, p, smallText);
-        body.append(heading, innerSmall);
+        body.append(heading);
 
         prevSearchDiv.append(anchor);
     }
 
     loadPreviousSearchesData()
+}
+
+function changeActive(event){
+    let anchorBoxes = $('a');
+
+    console.log(anchorBoxes);
+    for(let i = 0; i < anchorBoxes.length; i++){
+        if($(anchorBoxes[i]).hasClass("active")){
+            $(anchorBoxes[i]).removeClass('active');
+        }
+    }
+
+    let clickedBox = $(event.target).closest('a');
+
+    console.log(clickedBox)
+
+    $(clickedBox).addClass('active');
+
+    
+    $('#mainContainer').empty();
+    loadMainBoard();
 }
 
 function loadMainBoard(){
@@ -82,28 +121,43 @@ function loadMainBoard(){
 
     weatherDataFetch(currentSearch, "current")
         .then(data =>{
-
+        console.log(data)
                 //Get current weather data for selected city
 
         
 
         //Load today's weather 
-        let mainHeading = $('h2').text(currentSearch);
+        let mainHeading = $('<h2>').text(currentSearch).css("color", "white");
         mainContainer.append(mainHeading);
 
-        let mainCard = $('<div>').addClass('card');
+        let mainCard = $('<div>').addClass('card text-bg-dark');
         let headerCard = $('<div>').addClass('card-header');
-        let bodyCard = $('<div>').addClass('card-body');
+        let bodyCard = $('<div>').addClass('card-body row');
+        let mainTemps = $('<div>').addClass('col-6')
+        let otherInfo = $('<div>').addClass('col-6')
 
         mainCard.append(headerCard, bodyCard);
 
-        let smallHeading = $('<h5>').addClass('card-title').text("Today's weather");
-        let currentTemperature = $('<h5>').text("The current temperature is: " + data.main.temp)
-        let currentWind = $('<h5>').text("Wind: " + data.wind.speed);
-        let currentHumidity = $('<h5>').text("Humidity: " + data.main.humidity);
+        //Add icon
+        let iconDiv = $('<div>').attr('id', 'icon');
+        let iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        let iconImg = $('<img>').attr('src', iconUrl).attr('alt', "weather icon").attr('height', "100px");
+        iconDiv.append(iconImg);
+        bodyCard.append(iconDiv);
+        //Add weather info
+        let smallHeading = $('<h3>').addClass('card-title').text("Today's weather in " + currentSearch);
+        let currentTemperature = $('<h5>').text("The current temperature is: " + data.main.temp + "°C")
+        let currentWind = $('<h5>').text("Wind: " + data.wind.speed + " km/h");
+        let currentHumidity = $('<h5>').text("Humidity: " + data.main.humidity + " g.m-3");
+        let currentFeelsLike = $('<h5>').text("Feels like: " + data.main.feels_like + "°C");
+        let maxTemp = $('<h5>').text("Max: " + data.main.temp_max + "°C");
+        let minTemp = $('<h5>').text("Min: " + data.main.temp_min + "°C");
+
+        mainTemps.append(currentTemperature, currentFeelsLike, minTemp, maxTemp);
+        otherInfo.append(currentWind, currentHumidity)
 
         headerCard.append(smallHeading);
-        bodyCard.append(currentTemperature, currentWind, currentHumidity);
+        bodyCard.append(mainTemps, otherInfo);
         mainContainer.append(mainCard)
 
 
@@ -116,23 +170,31 @@ function loadMainBoard(){
         .then(data =>{
         console.log(data)
             //Load 5 day forecast 
-        let fiveDayHeader = $('<h3>').text("5 Day Forecast:");
+        let fiveDayHeader = $('<h3>').text("5 Day Forecast:").css('color', 'white').addClass(' mt-4');
         mainContainer.append(fiveDayHeader);
 
-        for(let i = 0; i < 5; i++){
-            let date = data.list[0].dt;
+        for(let i = 4; i < data.list.length; i = i + 8){
+            let date = data.list[i].dt;
+            console.log();
 
             
-            let mainForecastCard = $('<div>').addClass('card');
+            let mainForecastCard = $('<div>').addClass('card  col-lg-2 m-3 text-bg-warning col-sm-10 col-md-10');
             let headerForecastCard = $('<div>').addClass('card-header');
             let bodyForecastCard = $('<div>').addClass('card-body');
 
+            //Add Icon at top of card
+            let iconDiv = $('<div>').attr('id', 'icon');
+            let iconUrl = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
+            let iconImg = $('<img>').attr('src', iconUrl).attr('alt', "weather icon");
+            iconDiv.append(iconImg);
+            bodyForecastCard.append(iconDiv);
+
             mainForecastCard.append(headerForecastCard, bodyForecastCard);
 
-            let smallForecastHeading = $('<h5>').addClass('card-title').text(date);
-            let smallForecastTemp = $('<h5>').text(data.list[0].main.temp);
-            let smallForecastWind = $('<h5>').text(data.list[0].wind.speed);
-            let smallForecastHumidity = $('<h5>').text(data.list[0].main.humidity);
+            let smallForecastHeading = $('<h5>').addClass('card-title').text(dayjs.unix(date).format("dddd, DD/MM/YYYY"));
+            let smallForecastTemp = $('<p>').text("Temperature: " + data.list[i].main.temp + "°C");
+            let smallForecastWind = $('<p>').text("Wind speed: " + data.list[i].wind.speed + " km/h");
+            let smallForecastHumidity = $('<p>').text("Humidity: " + data.list[i].main.humidity + " g.m-3");
 
             headerForecastCard.append(smallForecastHeading);
             bodyForecastCard.append(smallForecastTemp, smallForecastWind, smallForecastHumidity);
@@ -159,12 +221,10 @@ function loadPreviousSearchesData(){
         weatherDataFetch(cityName, "current")
             .then(data =>{
                 let currentTempText = $(this).find('p');
-                currentTempText.text(data.main.temp + " Degrees")
+                currentTempText.text(data.main.temp + "°C")
 
                 let tempDesc = $(this).find('#weatherDescription');
                 tempDesc.text(data.weather[0].description)
-
-
             })
             .catch(error =>{
                 console.error("Error", error)
@@ -184,6 +244,7 @@ function weatherDataFetch(cityName, day){
             let longitude = data[0].lon;
             if(day === "current"){
                 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+ latitude + "&lon=" + longitude + "&appid=" + weatherAPI + "&units=metric";
+
             return fetch(apiUrl)
                 .then(response =>{
                     if(!response.ok){
@@ -195,13 +256,12 @@ function weatherDataFetch(cityName, day){
                 .then(weatherData =>{
                     return weatherData;
                 })
-                .catch(error =>{
+                .catch (error =>{
                     console.error('Error:', error)
                     throw error;
                 })
-                return data;
             } else if (day == "fiveDay"){
-                let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + weatherAPI + "&units=metric";
+                let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+ latitude + "&lon=" + longitude + "&appid=" + weatherAPI + "&units=metric";
 
                 return fetch(apiUrl)
                 .then(response =>{
@@ -218,7 +278,6 @@ function weatherDataFetch(cityName, day){
                     console.error('Error:', error)
                     throw error;
                 })
-                return data;
             }
         })
         
@@ -229,23 +288,23 @@ function weatherDataFetch(cityName, day){
     
 }
 
-async function coordinatesFetch(cityName){
-    
-    try{
-        let apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=6b83faa0314e5970068d0ef9b2e007ee";
+function coordinatesFetch(cityName){
+    let apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=6b83faa0314e5970068d0ef9b2e007ee";
 
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            throw new Error('Network response failed');
-        }
-
-        const data = await response.json();
-        return data
-    } catch(error){
-        console.error('Error', error);
-        throw error;
-    }
+    return fetch(apiUrl)
+        .then(response =>{
+            if(!response.ok){
+                throw new Error('Network response failed');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            return data
+        })
+        .catch(error => {
+            console.error("Error", error);
+            throw error;
+        })
     
 }
 
